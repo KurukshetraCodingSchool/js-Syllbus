af6051b50634b38667285ba34cf454cabe201ae0
Kurukshetra <kurukshetracodingschool@gmail.com>
2024-11-14 20:43:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,119 @@
+// var a = 5
+
+// 5 intiger value
+// 5.6 float value
+// "alok" / 'alok' String
+
+// true / false boolean value 
+// 5/6
+
+
+// var a = 5;
+// console.log(a);
+// var b = 5.6;
+// console.log(b);
+// console.log("jagbir")
+// console.log('jageshwar')
+// console.log(true)
+// console.log(false)
+
+// console.log(a)
+// var a = 25
+
+
+// var a = null
+// console.log(null)
+
+// var b = 25
+// console.log(b+"alok")
+
+// console.log(b*"alok")  // NaN Number * String
+// console.log("alok"*b) // NaN  String * Number
+
+
+// b is a number 
+// "alok" is a string
+
+
+
+// Operators
+
+// var a = 5; 
+// var b = 6;
+
+// var sum;  // sum is variable
+// sum  =  a+b;
+// console.log(sum); 
+
+
+// sum  =  a+b  // Operation
+
+// sum =  Result
+// a , b  = operant
+// + Operators
+
+// + - * / % ** ++ -- 
+
+
+// 8 types of Operators 
+// 1. Arithmetic Operators
+// 2. Assignment Operators
+// 3. Comparison Operators
+// 4. Logical Operators
+// 5. Bitwise Operators
+// 6. Ternary Operators
+// 7. String Operators
+// 8. Type Operators
+// 9. Nullish Coalescing Operators 
+
+// 1. Arithmetic Operators
+// + Addition
+// - Subtraction
+// * Multiplication
+// / Division
+// % Modulus(Remainder)
+// ++ Increment (Increases a value by 1)
+// --  Decrement (Decreases a value by 1)
+// ** Exponentiation (Raise to the power of base)
+
+
+// Increment Operators
+// Two type of Increment 
+// 1. Prefix (++a)
+// 2. Postfix (a++)
+
+
+// prefix
+var a = 10;
+console.log(++a) // First Increases than print
+// Output = 11
+
+// postfix
+var b = 10;
+console.log(b++) // First Print than Increases
+// Output = 10
+
+// Decrement Operators
+// Two type of Decrement
+// 1. Prefix (--a)
+// 2. Postfix (a--)
+
+// Prefix
+
+var c = 10;
+console.log(--c);
+// Output = 9
+
+//  Postfix
+var d = 10;
+console.log(d--);
+// Output = 10
+
+
+// ** Exponentiation
+
+var base = 2;
+var exponent = 3;
+var Result = base ** exponent;
+
+console.log(Result);
